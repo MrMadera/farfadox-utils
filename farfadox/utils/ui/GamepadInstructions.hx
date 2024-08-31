7762129a2ff182644ead4262f9896ac4d6d04285
MrMadera <ismaelglhs@gmail.com>
2024-08-31 18:36:52 +0200

    Basic actions added (based on gamepad ones)

--- farfadox/utils/ui/GamepadInstructions.hx
+++ farfadox/utils/ui/GamepadInstructions.hx
@@ -4,12 +4,42 @@ import flixel.FlxSprite;
 import flixel.text.FlxText;
 import flixel.group.FlxSpriteGroup;
 
+enum DeviceActions
+{
+    // Model: Xbox, PlayStation, Switch
+    SOUTH_BUTTON; // A, cross, B
+    WEST_BUTTON; // X, square, Y
+    NORTH_BUTTON; // Y, triangle, X
+    EAST_BUTTON; // B, circle, A
+
+    DIRECTIONAL_PAD_UP; // Up arrow
+    DIRECTIONAL_PAD_DOWN; // Down arrow
+    DIRECTIONAL_PAD_LEFT; // Left arrow
+    DIRECTIONAL_PAD_RIGHT; // Right arrow
+
+    LEFT_STICK;
+    RIGHT_STICK;
+
+    LEFT_STICK_BUTTON; 
+    RIGHT_STICK_BUTTON;
+
+    LEFT_BUMPER; //LB, L1, L2
+    RIGHT_BUMPER; //RB, R1, R2
+
+    LEFT_TRIGGER; //LT, L2, L2
+    RIGHT_TRIGGER; //RT, R2, R2
+
+    START__OPTIONS; // Start or options button
+    SELECT__SHARE__BACK; //Select, share of back button
+    HOME;
+}
+
 class GamepadInstructions extends FlxSpriteGroup
 {
     public var keyIcon:FlxSprite; //i'll change this for a custom class in the future
     public var instructionText:FlxText;
 
-    public function new(x:Float, y:Float, instructionText:String, textSize)
+    public function new(x:Float, y:Float, instructionText:String, textSize:Int, action:DeviceActions)
     {
         super(x, y);
 
