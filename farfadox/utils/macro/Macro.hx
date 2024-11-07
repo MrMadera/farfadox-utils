85c472a188507fa5bb185a79f45099fe12920912
MrMadera <90923803+MrMadera@users.noreply.github.com>
2024-11-07 22:06:20 +0000

    Building helper (BETA) (AWESOME TO DO: make a custom command to it, like farfadox-utils start or smth like that)

--- farfadox/utils/macro/Macro.hx
+++ farfadox/utils/macro/Macro.hx
@@ -13,7 +13,7 @@ class Macro {
     macro
     public static function initiateMacro()
     {
-        #if !debug
+        #if (!SKIP_MACRO)
         function checkAnswer()
         {
             if (["yes", "y"].contains(Sys.stdin().readLine().toLowerCase().trim()))
@@ -83,6 +83,67 @@ class Macro {
             {
                 log('Starting with current version.');
             }
+        }
+
+        Sys.print('Would you like to use the build helper? (BETA) (Y/N):');
+        var buildHelperRead = Sys.stdin().readLine();
+        if(["yes", "y"].contains(buildHelperRead.toLowerCase().trim()))
+        {
+            log('Helper started!');
+            log('');
+            Sys.print('Are you building or testing? (build/test):');
+            var buildTestRead = Sys.stdin().readLine();
+            var isBuilding:Bool;
+            if(["build", "b"].contains(buildTestRead.toLowerCase().trim()))
+            {
+                isBuilding = true;
+            }
+            else if(["test", "b"].contains(buildTestRead.toLowerCase().trim()))
+            {
+                isBuilding = false;
+            }
+
+            log('');
+            Sys.print('Which systems would you like to compile? (windows, mac, linux):');
+            var systemsRead = Sys.stdin().readLine();
+            if(["windows", "w"].contains(systemsRead.toLowerCase().trim()))
+            {
+                // windows shit
+                log('Bulding at ${Date.now()}');
+
+                var command:Dynamic;
+                if(isBuilding) command = Sys.command('lime build windows -D SKIP_MACRO');
+                else command = Sys.command('lime test windows -D SKIP_MACRO');
+
+                Sys.exit(1);
+            }
+            else if(["mac", "m"].contains(systemsRead.toLowerCase().trim()))
+            {
+                // mac whit
+                log('Bulding at ${Date.now()}');
+
+                var command:Dynamic;
+                if(isBuilding) command = Sys.command('lime build mac -D SKIP_MACRO');
+                else command = Sys.command('lime test mac -D SKIP_MACRO');
+
+                Sys.exit(1);
+            }
+            else if(["linux", "l"].contains(systemsRead.toLowerCase().trim()))
+            {
+                // linux shit
+                log('Bulding at ${Date.now()}');
+
+                var command:Dynamic;
+                if(isBuilding) command = Sys.command('lime build linux -D SKIP_MACRO');
+                else command = Sys.command('lime test linux -D SKIP_MACRO');
+
+                Sys.exit(1);
+            }
+
+        }
+        else if(["no", "n"].contains(buildHelperRead.toLowerCase().trim()))
+        {
+            log("Skipping building helper...");
         }
         #end
 
