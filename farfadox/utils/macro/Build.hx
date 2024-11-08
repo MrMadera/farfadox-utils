76a0f396b7afc572049159e76382115be7c9314e
MrMadera <90923803+MrMadera@users.noreply.github.com>
2024-11-08 16:48:27 +0000

    hehehe commands are workin' (TO DO: try to make custom commands recognized by the cmlet

---
+++ farfadox/utils/macro/Build.hx
@@ -0,0 +1,104 @@
+package farfadox.utils.macro;
+
+import sys.io.Process;
+
+using StringTools;
+
+class Build
+{
+    public static function main() 
+    {
+        var args = Sys.args();
+
+        if (args.length == 0) {
+            Sys.println("Usage: haxelib run <library-name> <command>");
+            return;
+        }
+
+        switch (args[0]) {
+            case "build":
+                build();
+            case "command2":
+                // Define another command
+                Sys.println("Executing command2...");
+            default:
+                Sys.println("Unknown command: " + args[0]);
+        }    
+    }
+
+    static function build()
+    {
+        Sys.print('Are you sure do you want to use the build helper (BETA)? (Y/N):');
+        var buildHelperRead = Sys.stdin().readLine();
+        if(["yes", "y"].contains(buildHelperRead.toLowerCase().trim()))
+        {
+            log('Helper started!');
+            log('');
+            Sys.print('Are you building or testing? (build/test):');
+            var buildTestRead = Sys.stdin().readLine();
+            var isBuilding:Bool = false;
+            if(["build", "b"].contains(buildTestRead.toLowerCase().trim()))
+            {
+                isBuilding = true;
+            }
+            else if(["test", "b"].contains(buildTestRead.toLowerCase().trim()))
+            {
+                isBuilding = false;
+            }
+
+            log('');
+            Sys.print('Which systems would you like to compile? (windows, mac, linux):');
+            var systemsRead = Sys.stdin().readLine();
+            if(["windows", "w"].contains(systemsRead.toLowerCase().trim()))
+            {
+                // windows shit
+                log('Bulding at ${Date.now()}');
+
+                var curDirectory = Sys.args().copy().pop();
+                log('Setting directory to $curDirectory');
+                Sys.setCwd(curDirectory);
+
+                var command:Dynamic;
+                if(isBuilding) command = Sys.command('lime build windows -D SKIP_MACRO');
+                else command = Sys.command('lime test windows -D SKIP_MACRO');
+
+                Sys.exit(1);
+            }
+            else if(["mac", "m"].contains(systemsRead.toLowerCase().trim()))
+            {
+                // mac whit
+                log('Bulding at ${Date.now()}');
+
+                var command:Dynamic;
+                if(isBuilding) command = Sys.command('lime build mac -D SKIP_MACRO');
+                else command = Sys.command('lime test mac -D SKIP_MACRO');
+
+                Sys.exit(1);
+            }
+            else if(["linux", "l"].contains(systemsRead.toLowerCase().trim()))
+            {
+                // linux shit
+                log('Bulding at ${Date.now()}');
+
+                var command:Dynamic;
+                if(isBuilding) command = Sys.command('lime build linux -D SKIP_MACRO');
+                else command = Sys.command('lime test linux -D SKIP_MACRO');
+
+                Sys.exit(1);
+            }
+        }
+        else if(["no", "n"].contains(buildHelperRead.toLowerCase().trim()))
+        {
+            log("Aborting...");
+            Sys.exit(1);
+        }
+    }
+
+    public static function log(?log:String = "") {
+        #if sys
+        Sys.println(log);
+        #else
+        trace('\n' + log);
+        #end
+    }
+}
